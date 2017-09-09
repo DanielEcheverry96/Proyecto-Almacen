eb040486650278c013b8feeeb908114de9ebf0c3
daniel <daniel@Daniel-Echeverry>
2017-09-08 20:52:07 -0500

    Menu Deportivos

--- src/vista/MenuCategoriaDeportivos.java
+++ src/vista/MenuCategoriaDeportivos.java
@@ -5,10 +5,41 @@
  */
 package vista;
 
+import java.util.Scanner;
+
 /**
  *
  * @author BP-LABREDES00
  */
 public class MenuCategoriaDeportivos {
     
+        
+    MenuRaquetas menraq = new MenuRaquetas();
+    MenuBicicletas menbici = new MenuBicicletas();
+    
+
+    
+    Scanner menu = new Scanner(System.in);
+    String condicion = "";
+
+    public void MenuRaquetas() {
+        do {
+
+            System.out.println("1- Raquetas");
+            System.out.println("2- Bicicletas");
+            int opcion = menu.nextInt();
+            switch (opcion) {
+                case 1:
+                   menraq.menuRaquetas();
+                    break;
+                case 2:
+                    menbici.menuBicicletas();
+                    break;
+                default:
+                    System.out.println("Opcion no permitida");
+            }
+            System.out.println("Desea retornar al menu categoria Deportiva S/s");
+            condicion = menu.next();
+        } while (condicion.equalsIgnoreCase("s"));
+    }
 }
