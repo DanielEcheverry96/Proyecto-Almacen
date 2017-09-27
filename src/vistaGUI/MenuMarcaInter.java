f1b1912c905ede32cc902152055a1c332c544a4d
Daniel <Daniel@DESKTOP-TST3VU5>
2017-09-27 17:17:13 -0500

    interface menu computador

--- src/vistaGUI/MenuMarcaInter.java
+++ src/vistaGUI/MenuMarcaInter.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package vista;
+package vistaGUI;
 
 import controlador.ManejadorObjetos;
 import javax.swing.JOptionPane;
