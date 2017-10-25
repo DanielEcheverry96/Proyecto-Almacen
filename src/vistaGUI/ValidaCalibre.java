2e1b6be9dce61d2b6aca69c39fae09335ae417b3
daniel <dfecheverryg@unal.edu.co>
2017-10-25 00:15:29 -0500

    Clases de validar campos

---
+++ src/vistaGUI/ValidaCalibre.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vistaGUI;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author danie
+ */
+public class ValidaCalibre {
+        public static boolean validaCalibre(String prueba){
+        boolean pruebaValido = false;
+        
+        Pattern pat = Pattern.compile("^-?[0-9]+([,\\.][0-9]*)?$"); //^-?[0-9]+([,\\.][0-9]*)?$
+        Matcher mat = pat.matcher(prueba);
+        pruebaValido = mat.find();
+        return pruebaValido;
+    }
+}
---
+++ src/vistaGUI/ValidaVelocidad.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vistaGUI;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author danie
+ */
+public class ValidaVelocidad {
+    public static boolean validaVelocidad(String prueba){
+        boolean pruebaValido = false;
+        
+        Pattern pat = Pattern.compile("^[0-9]+$");
+        //Pattern pat = Pattern.compile("^[\\w-]+(\\.[\\w-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+
+        Matcher mat = pat.matcher(prueba);
+        pruebaValido = mat.find();
+        return pruebaValido;
+    }
+}
