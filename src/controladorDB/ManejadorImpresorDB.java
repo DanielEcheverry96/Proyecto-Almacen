371b73e38d9d5c235d7aa86cc3dddd1f0dab644f
daniel <dfecheverryg@unal.edu.co>
2017-11-26 00:30:16 -0500

    se corrige error en impresores

--- src/controladorDB/ManejadorImpresorDB.java
+++ src/controladorDB/ManejadorImpresorDB.java
@@ -41,7 +41,7 @@ public class ManejadorImpresorDB implements ICRUDDB {
                 String sql = "insert into articulo(idarticulo, nombrearticulo,cantidad,color,precio,imagen,idmarca,idcategoria) values(" + temp.getIdArticulo() + "," + "'" + temp.getNombre() + "'" + "," + temp.getCantidad()
                         + "," + "'" + temp.getColor() + "'" + "," + temp.getPrecio() + "," + "'" + temp.getImagen() + "'" + "," + temp.getMar().getId() + "," + idcategoria + ");";
                 stmt.executeUpdate(sql);
-                sql = "insert into impresor(idarticulo,tipo_impre,interface_red_impre,paginas_min_impre,resolucion_impre) values(" + temp.getIdArticulo() + "," + "'" + temp.getTipo() + "'" + "," + "'" + temp.getInterfacered() + "'" + "," + temp.getPaginasminuto() + "," + "'" + temp.getResolucion() + ");";
+                sql = "insert into impresor(idarticulo,tipo_impre,interface_red_impre,paginas_min_impre,resolucion_impre) values(" + temp.getIdArticulo() + "," + "'" + temp.getTipo() + "'" + "," + "'" + temp.getInterfacered() + "'" + "," + temp.getPaginasminuto() + "," + "'" + temp.getResolucion() +"'"+ ");";
                 stmt.executeUpdate(sql);
                 insertado = true;
                 conpost.close();
